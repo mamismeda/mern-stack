c524c3949135831625ef4ccdef52d048934d98ed
mamismeda <amamismeda@gmail.com>
2023-01-28 17:39:38 +0400

    creating controller functions

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,33 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000!!!", process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to db");
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port 4000!!!", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
