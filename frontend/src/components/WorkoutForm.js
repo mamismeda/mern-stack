91cd74cd540f028acd2935b0547599ca4c0f0084
mamismeda <amamismeda@gmail.com>
2023-01-28 21:44:09 +0400

    highlited error fields

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutForm = () => {
-  const { dispatch } = useWorkoutsContext()
+  const { dispatch } = useWorkoutsContext();
 
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const hanldeSubmit = async (e) => {
     e.preventDefault();
@@ -25,14 +26,16 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
     if (response.ok) {
       setTitle("");
       setLoad("");
       setReps("");
       setError(null);
+      setEmptyFields([]);
       console.log("new workout added", json);
-      dispatch({type: "CREATE_WORKOUT", payload: json})
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
 
@@ -45,6 +48,7 @@ const WorkoutForm = () => {
         type="text"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className={emptyFields.includes("title") ? "error" : ""}
       />
 
       <label>Load (in Kg) :</label>
@@ -52,6 +56,7 @@ const WorkoutForm = () => {
         type="number"
         onChange={(e) => setLoad(e.target.value)}
         value={load}
+        className={emptyFields.includes("load") ? "error" : ""}
       />
 
       <label>Reps :</label>
@@ -59,6 +64,7 @@ const WorkoutForm = () => {
         type="number"
         onChange={(e) => setReps(e.target.value)}
         value={reps}
+        className={emptyFields.includes("reps") ? "error" : ""}
       />
 
       <button>Add workout</button>
--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,31 +1,34 @@
 import { createContext, useReducer } from "react";
 
-export const WorkoutsContext = createContext()
+export const WorkoutsContext = createContext();
 
 export const workoutReducer = (state, action) => {
   switch (action.type) {
-    case "SET_WORKOUTS" :
-        return {
-            workouts: action.payload
-        }
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
     case "CREATE_WORKOUT":
-        return {
-            workouts: [action.payload, ...state.workouts]
-        }
-    default : 
-     return state
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id),
+      };
+    default:
+      return state;
   }
-}
+};
 
-export const WorkoutsContextProvider = ({children}) => {
-    const [state, dispatch] = useReducer(workoutReducer, {
-        workouts: null
-    })
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutReducer, {
+    workouts: null,
+  });
 
-
-    return (
-        <WorkoutsContext.Provider value={{...state, dispatch}}>
-         { children }
-        </WorkoutsContext.Provider>
-    )
-}
+  return (
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
+  );
+};
