f43de77d570e0041066c96c6d0f82fc900863bb5
mamismeda <amamismeda@gmail.com>
2023-01-28 20:40:14 +0400

    starting to use react context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,15 @@
 import {useEffect, useState} from "react"
 
+// components
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
 const Home = () => {
     const [workouts, setWorkouts] = useState(null);
 
     useEffect(() => {
        const fetchWorkouts = async () => {
-        const response = await fetch('http://localhost:4000/api/workouts')
+        const response = await fetch('/api/workouts')
         const json = await response.json()
 
         if(response.ok) {
@@ -20,9 +24,10 @@ const Home = () => {
         <div className="home">
            <div className="workouts">
               {workouts && workouts.map((workout) => (
-                <p key={workout._id}>{workout.title}</p>
+                <WorkoutDetails key={workout._id} workout={workout}/>
               ))}
            </div>
+           <WorkoutForm />
         </div>
     )
 }
